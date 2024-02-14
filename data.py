cd0fc6de22da90a89d4caae97746e3cb79882f20
Goodman <10665385+liam0949@users.noreply.github.com>
2024-02-15 03:46:37 +0800

    First commit

--- data.py
+++ data.py
@@ -59,6 +59,10 @@ def load(task_name, tokenizer, shot=1000000000, max_seq_length=256, is_id=False)
     train_dataset = datasets["train"].map(preprocess_function, batched=True) if 'train' in datasets and is_id else None
     dev_dataset = datasets["validation"].map(preprocess_function, batched=True) if 'validation' in datasets and is_id else None
     test_dataset = datasets["test"].map(preprocess_function, batched=True) if 'test' in datasets else None
+    import pandas as pd
+
+    train_dataset.to_pandas().info()
+    dev_dataset.to_pandas().info()
     # train_dataset = list(map(preprocess_function, datasets['train'])) if 'train' in datasets and is_id else None
     # dev_dataset = list(map(preprocess_function, datasets['validation'])) if 'validation' in datasets and is_id else None
     # test_dataset = list(map(preprocess_function, datasets['test'])) if 'test' in datasets else None
