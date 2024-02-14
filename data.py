d464a598313ad350459a7417fac03a06c1355683
Goodman <10665385+liam0949@users.noreply.github.com>
2024-02-15 03:37:02 +0800

    First commit

--- data.py
+++ data.py
@@ -53,12 +53,15 @@ def load(task_name, tokenizer, shot=1000000000, max_seq_length=256, is_id=False)
                 examples[sentence1_key] + " " + examples[sentence2_key],)
         )
         result = tokenizer(*inputs, max_length=max_seq_length, truncation=True)
-        result["labels"] = examples["label"] if 'label' in examples else 0
+        # result["labels"] = examples["label"] if 'label' in examples else 0
         return result
-    print(type(datasets['train']))
-    train_dataset = list(map(preprocess_function, datasets['train'])) if 'train' in datasets and is_id else None
-    dev_dataset = list(map(preprocess_function, datasets['validation'])) if 'validation' in datasets and is_id else None
-    test_dataset = list(map(preprocess_function, datasets['test'])) if 'test' in datasets else None
+    # print(type(datasets['train']))
+    train_dataset = datasets["train"].map(preprocess_function, batched=True) if 'train' in datasets and is_id else None
+    dev_dataset = datasets["validation"].map(preprocess_function, batched=True) if 'validation' in datasets and is_id else None
+    test_dataset = datasets["test"].map(preprocess_function, batched=True) if 'test' in datasets else None
+    # train_dataset = list(map(preprocess_function, datasets['train'])) if 'train' in datasets and is_id else None
+    # dev_dataset = list(map(preprocess_function, datasets['validation'])) if 'validation' in datasets and is_id else None
+    # test_dataset = list(map(preprocess_function, datasets['test'])) if 'test' in datasets else None
     return train_dataset, dev_dataset, test_dataset
 
 
