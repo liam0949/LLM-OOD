300c314f0b99171bdd1783b84fda97a273081d26
Goodman <10665385+liam0949@users.noreply.github.com>
2024-03-24 04:00:31 +0800

    frist

--- T5ForFT.py
+++ T5ForFT.py
@@ -130,8 +130,8 @@ if __name__ == '__main__':
     parser.add_argument("--max_seq_length", default=512, type=int)
     parser.add_argument("--task_name", default="sst2", type=str)
 
-    parser.add_argument("--batch_size", default=64, type=int)
-    parser.add_argument("--val_batch_size", default=128, type=int)
+    parser.add_argument("--batch_size", default=8, type=int)
+    parser.add_argument("--val_batch_size", default=8, type=int)
     parser.add_argument("--learning_rate", default=5e-5, type=float)
     parser.add_argument("--learning_rate_vae", default=1e-3, type=float)
     parser.add_argument("--adam_epsilon", default=1e-8, type=float)
