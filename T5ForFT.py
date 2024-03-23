c94bee50699935837b782b75c66fc9c27ea28454
Goodman <10665385+liam0949@users.noreply.github.com>
2024-03-24 02:36:32 +0800

    frist

--- T5ForFT.py
+++ T5ForFT.py
@@ -78,7 +78,7 @@ def compute_metrics(eval_pred):
 
     logits, labels = eval_pred  # eval_pred is the tuple of predictions and labels returned by the model
     print("in metrics")
-    print(logits[0].size())
+    # print(logits[0].size())
     print(logits[1].size())
     print(labels.size())
     preds = np.argmax(logits, axis=1)
