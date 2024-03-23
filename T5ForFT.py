4e90a2579eb2c548ed2a97c76828b04e0a6cce7c
Goodman <10665385+liam0949@users.noreply.github.com>
2024-03-24 03:47:52 +0800

    frist

--- T5ForFT.py
+++ T5ForFT.py
@@ -179,7 +179,7 @@ if __name__ == '__main__':
         trust_remote_code = True
     )
     model.config.output_hidden_states = True
-    model.config.keys_to_ignore_at_inference = "hidden_states"
+    model.config.keys_to_ignore_at_inference = ["hidden_states"]
 
     # model.print_trainable_parameters()
     model = model.cuda()
