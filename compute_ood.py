008e7007e5fa3ee02a166a1c86abd291e786e07f
Goodman <10665385+liam0949@users.noreply.github.com>
2024-03-25 18:12:14 +0800

    first

--- compute_ood.py
+++ compute_ood.py
@@ -40,7 +40,7 @@ def detect_ood(model, dev_dataloader, test_dataset, benchmarks, data_collator):
     # print("in_scores",len(in_scores))
 
     for tag, ood_features in benchmarks:
-        dataloader = DataLoader(ood_features, batch_size=128, collate_fn=data_collator)
+        dataloader = DataLoader(ood_features, batch_size=64, collate_fn=data_collator)
         out_scores = compute_ood(dataloader, model, class_var, class_mean, norm_bank, all_classes)
         results = evaluate_ood(in_scores, out_scores)
         # print("ood result", results)
