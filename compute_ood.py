540880d583b4e667e0df1d8f89775a29b4ee8767
Goodman <10665385+liam0949@users.noreply.github.com>
2024-03-31 11:13:06 +0800

    first

--- compute_ood.py
+++ compute_ood.py
@@ -208,7 +208,7 @@ def prepare_ood(model, dataloader=None):
 
 if __name__ == '__main__':
     args = parse_args("test")
-    args.ib = False
+    args.ib = True
     out_dir = os.path.join(args.save_results_path, args.task_name, str(args.seed), str(args.ib))
     print(out_dir)
     out_dir = find_subdir_with_smallest_number(out_dir)
